e78c575d719f71318d0bd53c489f9e84eb822854
Richard Montoya <richard@savant-bpo.com>
2019-09-28 12:58:11 -0500

    better Path env

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,15 @@ set -g -x fish_greeting ''
 
 eval (starship init fish)
 
-set PATH $PATH /home/richard/.bin/
+set -x -U PATH $PATH /home/richard/.bin/
 for val in (ls -d ~/.bin/*/)
-        set PATH $PATH $val/
-        set PATH $PATH $val/bin/
+        set -gx PATH $PATH $val
+        set -gx PATH $PATH "$val""bin/"
 end
-set PATH $PATH /usr/local/go/bin/
-set PATH $PATH $HOME/.go/bin/
+set -gx PATH $PATH $HOME/.local/bin/
+
+set -x -U PATH $PATH /usr/local/go/bin/
+set -x -U PATH $PATH $HOME/.go/bin/
 
 set GOPATH $HOME/.go/
 set GOPATH $GOPATH $HOME/Documents/golang/
@@ -22,8 +24,8 @@ set -xU NVIM_GTK_NO_HEADERBAR 1
 set -xU NVIM_GTK_NO_WINDOW_DECORATION 1
 alias code="nvim-gtk"
 
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
+set PYENV_ROOT $HOME/.pyenv
+set PATH $PYENV_ROOT/bin $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
