b90cbf5d8ec1a63a976658d5b5cbc53a0e8c3fa0
Richard Montoya <richard.a.montoya.l@gmail.com>
2019-09-29 14:16:46 -0500

    add kitty suport for multiple machines

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 set -g -x fish_greeting ''
 
-eval (starship init fish)
-
 set -x -U PATH $PATH /home/richard/.bin/
 for val in (ls -d ~/.bin/*/)
         set -gx PATH $PATH $val
@@ -9,17 +7,21 @@ for val in (ls -d ~/.bin/*/)
 end
 set -gx PATH $PATH $HOME/.local/bin/
 
+set -gx PATH $PATH $HOME/.cargo/bin/
+
 set -x -U PATH $PATH /usr/local/go/bin/
 set -x -U PATH $PATH $HOME/.go/bin/
 
 set GOPATH $HOME/.go/
 set GOPATH $GOPATH $HOME/Documents/golang/
 
+
 function nvm
         bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
-set -xU TERMINAL "gnome-terminal"
+eval (starship init fish)
+
 set -xU NVIM_GTK_NO_HEADERBAR 1
 set -xU NVIM_GTK_NO_WINDOW_DECORATION 1
 alias code="nvim-gtk"
@@ -29,4 +31,4 @@ set PATH $PYENV_ROOT/bin $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
-alias cat="bat"
+set -gx TERMINAL "kitty"
